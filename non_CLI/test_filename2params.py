7076f594a380dff771eee6a04e94d076793b92ea
Eric Swanson <eswanson@contractor.usgs.gov>
2022-08-29 15:00:29 -0400

    Add in query to check data corresponding to unix time

--- non_CLI/test_filename2params.py
+++ non_CLI/test_filename2params.py
@@ -2,13 +2,13 @@
 
 from coastcamDBfuncs import *
 
-filename = "1636052400.Thu.Nov.04_19_00_00.GMT.2021.examplexx.Camera1.bright.jpg"
+filename = "1660734000.Wed.Aug.17_11_0_0.GMT.2022.examplexx.c1.snap.jpg"
 
 connection = DBConnectCSV("C:/Users/eswanson/OneDrive - DOI/Documents/Python/db_access.csv")
 
 params = filename2param(filename, connection)
 
-print(params.extrinsics[0])
-print(params.intrinsics[0])
-print(params.metadata[0])
-print(params.local_origin)
+##print(params.extrinsics[0])
+##print(params.intrinsics[0])
+##print(params.metadata[0])
+##print(params.local_origin)
